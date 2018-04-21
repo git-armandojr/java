1449b43fb412f196e28265fa2d592003e9bc19e5
git-armandojr <36749729+git-armandojr@users.noreply.github.com>
2018-04-21 20:44:48 -0300

    Add files via upload

---
+++ DuasLinhas.java
@@ -0,0 +1,6 @@
+class DuasLinhas{
+    public static void main(String[] args){
+        System.out.println("Primeira linha");
+        System.out.print("Segunda linha");
+    }
+}
---
+++ QuebraLinha.java
@@ -0,0 +1,5 @@
+class QuebraLinha{
+    public static void main(String[] args){
+        System.out.println("Primeira linha \nSegunda linha");
+    }
+}
